a1e6fc3ef85f81bc8c95d314e8f313837ce92bc8
Isaac Lyne <IsaactimothyLyne@gmail.com>
2020-05-16 10:41:19 +1200

    Update app.module.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { MatTableModule} from '@angular/material/table';
+import { MatDialog} from '@angular/material/dialog';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -15,6 +16,7 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
     AppRoutingModule,
     BrowserAnimationsModule
     ,MatTableModule
+    ,MatDialog
   ],
   providers: [],
   bootstrap: [AppComponent]
